ad0d6946fae28e9d4940fc576e99985f66f377cb
hosseinr56 <ranjbarihossein440@gmail.com>
2025-04-19 01:53:01 +0330

    add classes

---
+++ src/person.java
@@ -0,0 +1,14 @@
+public class person {
+    private String firstname;
+    private String lastname;
+    public person(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+    public String getFirstName() {
+        return firstname;
+    }
+    public String getLastName() {
+        return lastname;
+    }
+}
---
+++ src/student.java
@@ -0,0 +1,29 @@
+public class student extends person {
+    public student (String firstname, String lastname) {
+        super(firstname, lastname);
+    }
+    private int index = 0;
+    private int [] vahed = new int[100];
+    private double [] nomre = new double[100];
+
+    public void adding (int vahed, double nomre) {
+        this.vahed[index] = vahed;
+        this.nomre[index] = nomre;
+        this.index++;
+    }
+
+    public double average (){
+        double total = 0.0;
+        int vahed = 0;
+        for (int i = 0; i <= index-1; i++) {
+            total += this.vahed[i]*this.nomre[i];
+            vahed +=  this.vahed[i];
+        }
+        if (total == 0.0) {
+            return 0.0;
+        }
+        else {
+            return total/vahed;
+        }
+    }
+}
