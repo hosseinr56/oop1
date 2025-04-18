cadef0094ff011597e0336ea3aad65c2a819bb68
hosseinr56 <ranjbarihossein440@gmail.com>
2025-04-19 02:09:02 +0330

    adding profosor

--- src/Professor.java
+++ src/Professor.java
@@ -1,2 +1,18 @@
-public class Professor {
+public class Professor extends person {
+    private int jalase;
+    private double hogog;
+    public Professor (String firstname, String lastname, int jalase, double hogog) {
+        super(firstname, lastname);
+        this.jalase = jalase;
+        this.hogog = hogog;
+    }
+    public int getjalase () {
+        return jalase ;
+    }
+    public double gethogog () {
+        return hogog;
+    }
+    public double calculateSalary () {
+        return this.hogog * this.jalase;
+    }
 }
